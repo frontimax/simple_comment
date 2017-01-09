7ec595721e0a8ee5c2e11af136fc612a679b8384
kirsche <info@cherrystoned.com>
2017-01-09 12:42:31 +0100

    init added devise setup

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170108104204) do
+ActiveRecord::Schema.define(version: 20170109113838) do
 
   create_table "segments", force: :cascade do |t|
     t.string   "type"
@@ -25,15 +25,26 @@ ActiveRecord::Schema.define(version: 20170108104204) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "name",                                  null: false
-    t.string   "email",                                 null: false
-    t.string   "country",       default: "Deutschland"
-    t.string   "country_code",  default: "DE"
-    t.string   "currency",      default: "Euro"
-    t.string   "currency_code", default: "EUR"
-    t.boolean  "active",        default: true
-    t.datetime "created_at",                            null: false
-    t.datetime "updated_at",                            null: false
+    t.string   "name",                                           null: false
+    t.string   "email",                                          null: false
+    t.string   "country",                default: "Deutschland"
+    t.string   "country_code",           default: "DE"
+    t.string   "currency",               default: "Euro"
+    t.string   "currency_code",          default: "EUR"
+    t.boolean  "active",                 default: true
+    t.datetime "created_at",                                     null: false
+    t.datetime "updated_at",                                     null: false
+    t.string   "encrypted_password",     default: "",            null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,             null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
